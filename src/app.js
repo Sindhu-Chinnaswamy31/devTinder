c5e912846d2c3d456e954cdd5c471138cb403307
Sindhu C <sindhuc@LTP0373.local>
2025-08-10 15:50:52 +0530

    multiple-requet-handler

--- src/app.js
+++ src/app.js
@@ -1,35 +1,26 @@
 const server = require("express");
 
 const app = server();
-// to handle the request handler 
-// app.use() it will take two params one is router and another one is call back function
-// app.use("/", (req, res) => {
-//     res.send("This is home page");
-// });
 
-app.use("/home", (req, res) => {
-    res.send("This is home page");
+app.use("/hello", (req, res) => {
+    res.send("Hellooooooo sindhu");
 });
 
-app.post("/user", (req, res) => {
-    res.send({"name":"sindhu"});
+// if we want to run multiple request handler
+app.use("/test", 
+[(req, res, next) => {
+    next(); //it will call next request handler
+    res.send("router handler 1");
+}, (req, res, next) => {
+    res.send("router handler 2");
+    next()
+}], (req, res, next)=>{
+    res.send("router handler 3");
+    next();
+}, (req, res, next)=>{
+    res.send("router handler 4");
+    next();
 });
-
-app.get("/test", (req, res) => {
-    res.send("heloo sindhu");
-});
-
-app.post("/queryParams", (req, res) => {
-    res.send(req.query);
-});
-
-app.post("/queryParams/:userId/:password", (req, res) => {
-    res.send(req.params);
-});
-
-// app.get("/a(b?)c", (req, res) => {
-//     res.send("Matches /abc or /ac");
-// }); -> this will match /abc or /ac, but it wont work in express 5.X.X
 
 app.listen(8000, () =>{
     console.log("Server is running on port 8000");
